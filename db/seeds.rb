8726a908beae9e07a8064720ab8896ef3f14e508
samirajil <samirajeilinger@gmail.com>
2020-11-05 16:01:55 +0000

    first draft seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+gem "byebug"
+
+InputMistake.delete_all
+Alternative.delete_all
+Mistake.delete_all
+
+list = [
+    {
+        :mistake => "mankind",
+        :alternatives => ["humankind", "humanity"]
+    },
+    {
+        :mistake => "chairman",
+        :alternatives => ["chair", "chairperson"]
+    },
+]
+
+list.each do |item|
+  @mistake = Mistake.create(mistaken_word: item[:mistake])
+  @mistake.save
+  item[:alternatives].each do |alternative|
+    @alternative = Alternative.create(mistake: @mistake, alternative_word: alternative)
+    @alternative.save
+  end
+end
