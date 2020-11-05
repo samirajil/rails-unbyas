634849ecb6a9d72d98a2d75a141974a1ab249567
samirajil <samirajeilinger@gmail.com>
2020-11-05 16:47:19 +0000

    controller

--- app/controllers/inputs_controller.rb
+++ app/controllers/inputs_controller.rb
@@ -5,6 +5,11 @@ class InputsController < ApplicationController
 
   def create
     @input = Input.find(params[:input_id])
+    if @input.save
+      redirect_to 
+    else
+      render :new
+    end
   end
 
 end
